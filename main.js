bff93f9e1e5c67d5515b70713740559c4cc3e8a1
Guillermo-C <ninjamemo@Guillermos-MacBook-Pro.local>
2017-06-03 19:57:44 -0700

    Able to add nodes properly

--- main.js
+++ main.js
@@ -48,40 +48,119 @@ printMyName("Memo")*/
     };
 }*/
 
+/**
+ *
+ * @class BinarySearchTree
+ * @constructor
+ */
 
 function BinarySearchTree() {
+
+    /**
+     *
+     * @property root -Is the root
+     * @type Object
+     *
+     */
     this.root = null;
+
+
+
+
+
 }
 
-BinarySearchTree.prototype= {
+BinarySearchTree.prototype = {
 
     //  Add a node to the binary tree.
     add: function () {
+
+        //  Request node value.
+        var value = parseInt(prompt("Enter int value: ","0"), 10);
 
         //  Create node.
         var node = {
             value : value,
             leftNode: null,
-            rightNode: null
-        }
-
-        //  Request node value.
-        var value = prompt("Enter int value: ", 0);
-
+            rightNode: null,
+            respectiveRoot: 0
+        },
+            current;            //  Reference this new node as 'current'.
 
         node.value = value;
 
 
         // Make current node the root, if root is empty.
-        if(this.root === null){
+        if(this.root == null){
             this.root = node;
             console.log("Node: " + node.value + " was added as root")
         }
-        alert("Node value entered: " + node.value);
+        else{
+
+            current = this.root;
+
+            while(true){
+
+                //  Value is less than current node value. Go left.
+                if(value < current.value){
+
+                    if(current.leftNode == null){
+                        current.leftNode = node;
+                        node.respectiveRoot = current.value; // Test
+                        console.log("Node: " + node.value + " added to the left");
+                        console.log("respectiveRoot: " + node.respectiveRoot);
+                        break;
+                    }
+                    else{
+                        current = current.leftNode;
+                    }
+
+
+                }
+                //  Value is more than current node value. Go right.
+                else if (value > current.value){
+
+                    if(current.rightNode == null){
+                        current.rightNode = node;
+                        node.respectiveRoot = current.value;
+                        console.log("Node: " + node.value + " added to the right");
+                        console.log("respectiveRoot: " + node.respectiveRoot);
+                        break;
+                    }
+                    else{
+                        current = current.rightNode;
+                    }
+
+                }
+                //  If value entered is equal to previously entered number, just ignore it.
+                else{
+                    console.log("Duplicate value,"+ value +" ignored!");
+                    break;
+                }
+            }
+
+        }
+
+        console.log("--------");
+
+    },
+
+    //  Print all the elements pre-order format.
+    preOrder : function(node) {
+
+        if(node!= null){
+            console.log(node.value + " ");
+            preOrder(node.leftNode);
+            preOrder(node.rightNode);
+        }
+
+
+        //current = this.root;
+
     }
 
 
-}
+};
 
 
 
